f0e751643a5fe895c53c6228aea6f05bb650004d
Jihoi-Kang <jihoi.kang@gmail.com>
2020-12-09 15:25:33 +0900

    Implement google map

--- app/src/main/java/com/jay/friends/ui/detail/UserDetailActivity.kt
+++ app/src/main/java/com/jay/friends/ui/detail/UserDetailActivity.kt
@@ -3,15 +3,22 @@ package com.jay.friends.ui.detail
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.jay.friends.R
 import com.jay.friends.base.BaseActivity
 import com.jay.friends.databinding.ActivityUserDetailBinding
 import com.jay.friends.model.UserModel
 
+
 class UserDetailActivity : BaseActivity<UserDetailViewModel, ActivityUserDetailBinding>(
     R.layout.activity_user_detail,
     UserDetailViewModel::class.java
-) {
+), OnMapReadyCallback {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +28,18 @@ class UserDetailActivity : BaseActivity<UserDetailViewModel, ActivityUserDetailB
 
     private fun setupView() {
         binding.item = intent.getParcelableExtra(EXTRA_USER_MODEL)
+
+        (supportFragmentManager.findFragmentById(R.id.map) as? SupportMapFragment)
+            ?.getMapAsync(this@UserDetailActivity)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap?) {
+        googleMap ?: return
+        val item = binding.item ?: return
+
+        val location = LatLng(item.location.lat, item.location.lng)
+        googleMap.addMarker(MarkerOptions().position(location).title("Location"))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(location))
     }
 
     companion object {
